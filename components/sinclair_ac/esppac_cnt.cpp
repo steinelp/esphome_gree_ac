e503d1b109064e7e8b97199413102b0745725980
gekkehenkie11 <gekkehenkie11@users.noreply.github.com>
2025-07-05 16:34:04 -0400

    Update esppac_cnt.cpp

--- components/sinclair_ac/esppac_cnt.cpp
+++ components/sinclair_ac/esppac_cnt.cpp
@@ -497,7 +497,7 @@ void SinclairACCNT::send_packet()
 
     for (int i = 0; i < 20; i++)
          lastpacket[i] = packet[i];
-    ESP_LOGV(TAG, "Sending new packet now");
+    
     packet.insert(packet.begin(), protocol::CMD_OUT_PARAMS_SET);
     packet.insert(packet.begin(), protocol::SET_PACKET_LEN + 2); /* Add 2 bytes as we added a command and will add checksum */
 
