1bcb3052916924179a3d67f8930d7d34c9db3a86
gekkehenkie11 <gekkehenkie11@users.noreply.github.com>
2025-07-05 12:15:44 -0400

    Update esppac_cnt.h

--- components/sinclair_ac/esppac_cnt.h
+++ components/sinclair_ac/esppac_cnt.h
@@ -202,7 +202,7 @@ class SinclairACCNT : public SinclairAC {
         bool processUnitReport();
 
         void send_packet();
-        //std::vector<uint8_t> lastpacket;
+        std::vector<uint8_t> lastpacket;
         unsigned char abc[3];
 
         bool verify_packet();
