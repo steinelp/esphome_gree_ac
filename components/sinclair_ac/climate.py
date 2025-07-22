bfac1ff8489f91723e3b6155f80cd2ac68e6282b
steinelp <63597548+steinelp@users.noreply.github.com>
2025-07-22 23:38:05 +0200

    Update climate.py

--- components/sinclair_ac/climate.py
+++ components/sinclair_ac/climate.py
@@ -76,14 +76,14 @@ DISPLAY_UNIT_OPTIONS = [
     "F",
 ]
 
-switch_schema = switch.switch_schema.extend(cv.COMPONENT_SCHEMA).extend(
+switch_schema = switch.switch_schema(switch.Switch).extend(cv.COMPONENT_SCHEMA).extend(
     {cv.GenerateID(): cv.declare_id(SinclairACSwitch)}
 )
-select_schema = select.select_schema.extend(
+select_schema = select.select_schema(select.Select).extend(
     {cv.GenerateID(CONF_ID): cv.declare_id(SinclairACSelect)}
 )
 
-SCHEMA = climate.climate_schema.extend(
+SCHEMA = climate.climate_schema(climate.Climate).extend(
     {
         cv.Optional(CONF_HORIZONTAL_SWING_SELECT): select_schema,
         cv.Optional(CONF_VERTICAL_SWING_SELECT): select_schema,
