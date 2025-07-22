573ca6b3b05546cc4ad73c13fe99559a7eb7bf0b
steinelp <63597548+steinelp@users.noreply.github.com>
2025-07-22 22:31:19 +0200

    Update climate.py
    update variable names

--- components/sinclair_ac/climate.py
+++ components/sinclair_ac/climate.py
@@ -76,10 +76,10 @@ DISPLAY_UNIT_OPTIONS = [
     "F",
 ]
 
-SWITCH_SCHEMA = switch.switch_schema.extend(cv.COMPONENT_SCHEMA).extend(
+switch_schema = switch.switch_schema.extend(cv.component_schema).extend(
     {cv.GenerateID(): cv.declare_id(SinclairACSwitch)}
 )
-SELECT_SCHEMA = select.select_schema.extend(
+select_schema = select.select_schema.extend(
     {cv.GenerateID(CONF_ID): cv.declare_id(SinclairACSelect)}
 )
 
